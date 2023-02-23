7e770f6181130e44749da95fc5005a9cc18e6220
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-23 18:07:54 +0300

    added new feature module for week weather, renamed other feature modules

--- app/src/main/java/ru/msokolov/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/msokolov/weatherapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package ru.msokolov.weatherapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import ru.msokolov.weatherapp.api.ApiFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiService = ApiFactory.weatherApiService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MainScope().launch {
+            val answer = apiService.getWeatherForecast(55.755864, 37.617698)
+            Log.d("TAGMainActivity", answer.toString())
+        }
     }
 }
