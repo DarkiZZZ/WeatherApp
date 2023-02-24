4d71f68a1979711adebfbd50627cb5a2d58812cd
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-24 10:19:34 +0300

    managed color for themes, removed action bar, added theme to manifest

--- app/src/main/java/ru/msokolov/weatherapp/MainFragment.kt
+++ app/src/main/java/ru/msokolov/weatherapp/MainFragment.kt
@@ -2,10 +2,12 @@ package ru.msokolov.weatherapp
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import ru.msokolov.weatherapp.databinding.FragmentMainBinding
+
 
 class MainFragment: Fragment(R.layout.fragment_main) {
 
@@ -14,6 +16,7 @@ class MainFragment: Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
 
         val navHost = childFragmentManager.findFragmentById(R.id.tabsContainer) as NavHostFragment
         val navController = navHost.navController
