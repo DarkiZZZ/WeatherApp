a698fa1ff1312d0e8a499b5971f72309a2543662
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-03-22 22:43:33 +0300

    initialized in (feature:tomorrow): Fragment with View model

---
+++ feature/today/src/main/java/ru/msokolov/weatherapp/today/presentation/ui/TodayFragment.kt
@@ -0,0 +1,32 @@
+package ru.msokolov.weatherapp.today.presentation.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.msokolov.weatherapp.today.R
+
+class TodayFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TodayFragment()
+    }
+
+    private lateinit var viewModel: TodayViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_today, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(TodayViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
